0086e7b49b4ef1be3ce118ce1d15e1feb24c4d92
unknown <jochemn@10020i5.acsi.lan>
2014-05-27 16:23:02 +0200

    Added stylFixed some issues with native support and animation of the details content.
    In Chrome, which natively supports details, the animation would not
    work.Fixed this by removing open.details and close.details events,
    preventing the default click behavior on summary and adding classes
    correct when open.details or details.animated event is fired.

--- jquery.details.js
+++ jquery.details.js
@@ -1,179 +1,178 @@
 /*!Denis Sokolov, http://akral.github.io/details-tag/, denis@sokolov.cc. GPL, MIT */
 (function($){
-	var support = 'open' in document.createElement('details'), init;
+    var support = 'open' in document.createElement('details'), init;
 
-	// API
-	$.fn.details = function(command) {
-		if (command === 'open')
-		{
-			if (support)
-				this.prop('open', true);
-			else
-				this.trigger('open');
-		}
+    // API
+    $.fn.details = function(command) {
+        if (command === 'open')
+        {
+            if (support)
+                this.prop('open', true);
+            else
+                this.trigger('open');
+        }
 
-		if (command === 'close')
-		{
-			if (support)
-				this.prop('open', false);
-			else
-				this.trigger('close');
-		}
+        if (command === 'close')
+        {
+            if (support)
+                this.prop('open', false);
+            else
+                this.trigger('close');
+        }
 
-		if (command === 'init')
-			init($(this));
+        if (command === 'init')
+            init($(this));
 
-		if (!command) {
-			if (!support)
-				return this.hasClass('open');
+        if (!command) {
+            if (!support)
+                return this.hasClass('open');
 
-			var open = false;
-			this.each(function(){
-				if (this.open) {
-					open = true;
-					return false;
-				}
-			});
-			return open;
-		}
-	};
+            var open = false;
+            this.each(function(){
+                if (this.open) {
+                    open = true;
+                    return false;
+                }
+            });
+            return open;
+        }
+    };
 
-	init = function(elements) {
-		elements = elements.not('.details-inited').addClass('details-inited');
+    init = function(elements) {
+        elements = elements.not('.details-inited').addClass('details-inited');
 
-		/* Animated tags */
-		elements.filter('.animated').each(function(){
-			var me = $(this);
-			var summary = me.children('summary').remove();
-			var wrapper = $('<div>').addClass('details-wrapper').append(me.children());
-			me.append(wrapper).prepend(summary);
-		});
+        /* Animated tags */
+        elements.filter('.animated').each(function(){
+            var me = $(this);
+            var summary = me.children('summary').remove();
+            var wrapper = $('<div>').addClass('details-wrapper hide').append(me.children());
+            me.append(wrapper).prepend(summary);
+        });
 
-		if (support)
-			return;
+        if (support)
+            return;
 
-		elements
-			// Add missing summary tags
-			.each(function(){
-				var me = $(this);
-				if (!me.children('summary').length)
-					me.prepend('<summary>Details</summary>');
-			})
+        elements
+            // Add missing summary tags
+            .each(function(){
+                var me = $(this);
+                if (!me.children('summary').length)
+                    me.prepend('<summary>Details</summary>');
+            })
 
-			.children('summary')
-				.filter(':not(tabindex)').attr('tabindex', 0).end()
-			.end()
+            .children('summary')
+            .filter(':not(tabindex)').attr('tabindex', 0).end()
+            .end()
 
-			// Wrap plain text nodes in spans for CSS visibility
-			.contents(':not(summary)').filter(function(){
-					return (this.nodeType === 3) && (/[^\t\n\r ]/.test(this.data));
-				})
-				.wrap('<span>')
-			.end().end()
+            // Wrap plain text nodes in spans for CSS visibility
+            .contents(':not(summary)').filter(function(){
+                return (this.nodeType === 3) && (/[^\t\n\r ]/.test(this.data));
+            })
+            .wrap('<span>')
+            .end().end()
 
-			// Initial positions
-			.filter(':not([open])')
-				.prop('open', false)
-			.end()
-			.filter('[open]')
-				.addClass('open')
-				.prop('open', true)
-			.end();
+            // Initial positions
+            .filter(':not([open])')
+            .prop('open', false)
+            .end()
+            .filter('[open]')
+            .addClass('open')
+            .prop('open', true)
+            .end();
 
-		elements.filter(':not(.open)').children().not('summary').hide();
-	};
+        elements.filter(':not(.open)').children().not('summary').hide();
+    };
 
-	$(function(){
-		$('body')
-		.on('open.details', 'details.animated', function(){
-			var details = $(this);
-			var wrapper = details.children('.details-wrapper');
-			wrapper.hide();
-			setTimeout(function(){ // Simple .height() redraw is not enough for Chrome
-				wrapper.slideDown(details.data('animation-speed'));
-			}, 0);
-		})
-		.on('close.details', 'details.animated', function(){
-			var details = $(this);
-			var wrapper = details.children('.details-wrapper');
-			setTimeout(function(){
-				details.prop('open', true).addClass('open');
-				wrapper.slideUp(details.data('animation-speed'), function(){
-					details.prop('open', false).removeClass('open');
-				});
-			}, 0);
-		});
+    $(function(){
+        $('body')
+            .on('open.details', 'details.animated', function(){
+                var details = $(this);
+                var wrapper = details.children('.details-wrapper');
+                details.prop('open', true).addClass('open');
+                setTimeout(function(){ // Simple .height() redraw is not enough for Chrome
+                    wrapper.slideDown(details.data('animation-speed'));
+                }, 0);
+            })
+            .on('close.details', 'details.animated', function(){
+                var details = $(this);
+                var wrapper = details.children('.details-wrapper');
+                setTimeout(function(){
+                    wrapper.slideUp(details.data('animation-speed'), function(){
+                        details.prop('open', false).removeClass('open');
+                    });
+                }, 0);
+            });
 
-		if (support)
-		{
-			// Add our triggers on native implementation
-			$('body').on('click', 'summary', function(){
-				var details = $(this).parent();
-				setTimeout(function(){
-					if (details.prop('open'))
-						details.trigger('close');
-					else
-						details.trigger('open');
-				}, 0);
-			});
-			return;
-		}
+        if (support)
+        {
+            init($('details'));
 
-		// Everything below is a polyfill
+            // Add our triggers on native implementation
+            $('body').on('click', 'summary', function(e){
+                e.preventDefault();
 
-		$('html').addClass('no-details');
+                var details = $(this).parent();
+                setTimeout(function(){
+                    if (details.prop('open'))
+                        details.trigger('close');
+                    else
+                        details.trigger('open');
+                }, 0);
+            });
+            return;
+        }
 
-		$('head').prepend('<style>'+
-			// Style
-			'details{display:block}'+
-			'summary{cursor:pointer}'+
-			'details>summary::before{content:"►"}'+
-			'details.open>summary::before{content:"▼"}'+
-			'</style>'
-		);
+        // Everything below is a polyfill
 
-		$('body')
+        $('html').addClass('no-details');
 
-		.on('open.details', 'details', function(e){
-			$(this).addClass('open').trigger('change.details');
-			e.preventDefault();
-			e.stopPropagation();
-		})
+        /* Better to move this to a separate stylesheet so it can be minified */
+        $('head').prepend('<style>'+
+            // Style
+            'details{display:block}'+
+            'summary{cursor:pointer}'+
+            'details>summary::before{content:"?"}'+
+            'details[open]>summary::before,details.open>summary::before{content:"?"}'+
+            '.no-details details>*:not(summary){display:none}'+
+            'summary::-webkit-details-marker {display:none}'+
+            '.hide{display:none}'+
+            '</style>'
+        );
 
-		.on('close.details', 'details', function(e){
-			$(this).removeClass('open').trigger('change.details');
-			e.preventDefault();
-			e.stopPropagation();
-		})
+        $('body')
 
-		.on('toggle.details', 'details', function(e){
-			var me = $(this);
-			if (me.hasClass('open'))
-				me.trigger('close');
-			else
-				me.trigger('open');
-			e.stopPropagation();
-		})
+            .on('open.details', 'details', function(e){
+                $(this).addClass('open').trigger('change.details');
+                e.preventDefault();
+                e.stopPropagation();
+            })
 
-		.on('click', 'summary', function(){
-			$(this).parent().trigger('toggle');
-		})
+            .on('close.details', 'details', function(e){
+                $(this).removeClass('open').trigger('change.details');
+                e.preventDefault();
+                e.stopPropagation();
+            })
 
-		.on('keyup', 'summary', function(e){
-			// 32 - space
-			// 13 - Enter. Opera triggers .click()
-			if (e.keyCode === 32 || e.keyCode === 13)
-				$(this).parent().trigger('toggle');
-		});
+            .on('toggle.details', 'details', function(e){
+                var me = $(this);
+                if (me.hasClass('open'))
+                    me.trigger('close');
+                else
+                    me.trigger('open');
+                e.stopPropagation();
+            })
 
-		$('body')
-			.on('open.details', 'details', function(){
-				$(this).children().not('summary').show();
-			})
-			.on('close.details', 'details', function(){
-				$(this).children().not('summary').hide();
-			});
+            .on('click', 'summary', function(){
+                $(this).parent().trigger('toggle');
+            })
 
-		init($('details'));
-	});
+            .on('keyup', 'summary', function(e){
+                // 32 - space
+                // 13 - Enter. Opera triggers .click()
+                if (e.keyCode === 32 || e.keyCode === 13)
+                    $(this).parent().trigger('toggle');
+            });
+
+        init($('details'));
+    });
 })(jQuery);
